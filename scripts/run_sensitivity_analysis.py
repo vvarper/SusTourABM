217ff22380701ed9896c7280fe77be93fde1fb54
vvarper <vavp73@gmail.com>
2023-05-18 15:43:42 +0200

    feat: add Climatic Policy module and a script to perform a sensitivity analysis

---
+++ scripts/run_sensitivity_analysis.py
@@ -0,0 +1,68 @@
+import os
+import sys
+
+import numpy as np
+from SALib.analyze import sobol as sobol_analyze
+from SALib.sample import sobol as sobol_sample
+from matplotlib import pyplot as plt
+
+from sustourabm.climatic_policy.climatic_policy import ClimaticPolicy
+from sustourabm.util.io import load_model_instance_from_json
+
+# Process arguments from command line #########################################
+
+try:
+    instance = sys.argv[1]
+    destination_id = int(sys.argv[2])
+    n = int(sys.argv[3])
+    policy_start = int(sys.argv[4])
+    mc = int(sys.argv[5])
+    num_processes = int(sys.argv[6])
+
+except IndexError:
+    print(
+        "{0} <instance> <destination_id> <n> <start> <mc> <num_processes>".format(
+            sys.argv[0]))
+    sys.exit(1)
+
+# Example from SALib ##########################################################
+
+seed = 121
+instance_path = 'data/instances/' + instance + '.json'
+instance_name = os.path.split(instance)[1].replace('_instance', '')
+parameters, climate_factors, destinations = load_model_instance_from_json(
+    instance_path)
+
+problem = ClimaticPolicy(destination_id, destinations[destination_id],
+                         climate_factors, parameters, policy_start,
+                         parameters['num_steps'], mc, num_processes)
+
+# Generate parameter samples ##################################################
+
+param_values = sobol_sample.sample(problem.problem, n, seed=seed)
+sample_output = f'data/sensitivity_analysis/sample_params_n{n}.txt'
+os.makedirs(os.path.dirname(sample_output), exist_ok=True)
+np.savetxt(sample_output, param_values)
+
+# Evaluate the parameters of the sample #######################################
+
+Y = np.zeros([param_values.shape[0]])
+for i, X in enumerate(param_values):
+    Y[i] = problem.evaluate(X)
+
+eval_output = f'data/sensitivity_analysis/sample_evals_n{n}_{instance_name}' \
+              f'_{destinations[destination_id]}_{policy_start}' \
+              f'_{mc}_{num_processes}.txt'
+os.makedirs(os.path.dirname(eval_output), exist_ok=True)
+np.savetxt(eval_output, Y)
+
+# Perform the sensitivity analysis ############################################
+
+Si = sobol_analyze.analyze(problem.problem, Y, print_to_console=True)
+
+Si.plot()
+plot_output = f'data/sensitivity_analysis/sobol_n{n}_{instance_name}' \
+              f'_{destinations[destination_id]}_{policy_start}' \
+              f'_{mc}_{num_processes}.png'
+os.makedirs(os.path.dirname(plot_output), exist_ok=True)
+plt.savefig(plot_output)
---
+++ sustourabm/climatic_policy/climatic_policy.py
@@ -0,0 +1,55 @@
+import numpy as np
+
+from sustourabm.runner import simulate
+from sustourabm.util.io import totuple
+
+
+class ClimaticPolicy():
+
+    def __init__(self, destination_id, destination_name, factors_names,
+                 instance_parameters, policy_start, policy_end, mc,
+                 num_processes):
+        self.destination_id = destination_id
+        self.destination_name = destination_name
+        self.factors_names = factors_names
+        self.instance_parameters = instance_parameters
+        self.policy_start = policy_start
+        self.policy_end = policy_end
+        self.mc = mc
+        self.num_processes = num_processes
+        self.problem = {'names': factors_names,
+                        'bounds': [[0, 1]] * len(factors_names),
+                        'num_vars': len(factors_names),
+                        'outputs': ['Final share']}
+
+    def evaluate(self, solution):
+        policy_parameters = self.get_parameters_from_solution(solution)
+
+        results = simulate(parameters=policy_parameters, mc=self.mc,
+                           num_processes=self.num_processes)
+
+        result = results.summary_results[
+            'Share ' + str(self.destination_id), 'mean'].iloc[
+            self.policy_end - 1]
+
+        return result
+
+    def get_parameters_from_solution(self, solution):
+        policy_parameters = self.instance_parameters.copy()
+        states = np.array(
+            policy_parameters['state_by_destination_step_factor'][0])
+
+        original_start_share = states[self.destination_id, self.policy_start]
+        original_final_share = states[self.destination_id, self.policy_end - 1]
+        deterioration = np.maximum(original_final_share - original_start_share,
+                                   0)
+        new_final_share = solution * deterioration + original_start_share
+
+        states[self.destination_id,
+        self.policy_start:self.policy_end] = np.linspace(original_start_share,
+                                                         new_final_share,
+                                                         self.policy_end - self.policy_start)
+        policy_parameters['state_by_destination_step_factor'] = [
+            totuple(states)]
+
+        return policy_parameters
